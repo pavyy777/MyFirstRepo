d235a712af39d94a807bb143fac65a726ae18764
pavyy777 <ubpavan1998@gmail.com>
2021-12-29 15:21:26 +0530

    new file added

---
+++ HandsOnLogic/HandsOnLogic/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandsOnLogic
+{
+    class Program
+    {
+       public static void Main(string[] args)
+        {
+            RevEachWordString.reveachword();
+           // RevString revobj = new RevString();
+            //RevString.rev();
+           //ReverseString rs = new ReverseString();
+           // rs.Reverse();
+            //handson h = new handson();
+
+            //h.OccCount();
+            Console.ReadLine();
+            Console.WriteLine();
+
+
+        
+        }
+    }
+    }
+
